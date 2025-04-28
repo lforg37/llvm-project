453a6a6210f9ee2c3fb894c28745b2395743fbb6
Luc Forget <luc.forget@woven.toyota>
2025-04-28 11:14:49 +0900

    [mlir][wasm] Function operation for WasmDialect
    - Definition of `wasm.func` operation
    - Handling of the Function section in Wasm to MLIR importer

--- mlir/lib/Dialect/WebAssembly/IR/WebAssemblyOps.cpp
+++ mlir/lib/Dialect/WebAssembly/IR/WebAssemblyOps.cpp
@@ -2,6 +2,7 @@
 
 
 #include "mlir/IR/Builders.h"
+#include "mlir/IR/BuiltinAttributes.h"
 
 //===----------------------------------------------------------------------===//
 // TableGen'd op method definitions
@@ -9,3 +10,9 @@
 
 #define GET_OP_CLASSES
 #include "mlir/Dialect/WebAssembly/IR/WebAssemblyOps.cpp.inc"
+
+void mlir::wasm::FuncOp::build(::mlir::OpBuilder &odsBuilder, ::mlir::OperationState &odsState, llvm::StringRef symbol, FunctionType funcType) {
+  odsState.addAttribute("sym_name", odsBuilder.getStringAttr(symbol));
+  odsState.addAttribute("functionType", TypeAttr::get(funcType));
+  odsState.addRegion();
+}
