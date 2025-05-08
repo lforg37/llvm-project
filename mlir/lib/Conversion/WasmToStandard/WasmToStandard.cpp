c296800ea1e3bf0e87a9ff37531aad29e5dfc55f
Luc Forget <luc.forget@woven.toyota>
2025-05-08 16:30:06 +0900

    [mlir][wasm] WasmToStandard implementation of funcOp conversion

--- mlir/lib/Conversion/WasmToStandard/WasmToStandard.cpp
+++ mlir/lib/Conversion/WasmToStandard/WasmToStandard.cpp
@@ -15,8 +15,10 @@
 
 #include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
 #include "mlir/Dialect/Vector/IR/VectorOps.h"
 #include "mlir/Dialect/WebAssembly/IR/WebAssembly.h"
+#include "mlir/IR/BuiltinDialect.h"
 #include "mlir/Transforms/Passes.h"
 
 
@@ -29,19 +31,59 @@ namespace mlir {
 } // namespace mlir
 
 using namespace mlir;
+using namespace mlir::wasm;
 
 namespace {
+
+struct WasmFuncOpConversion : OpConversionPattern<FuncOp> {
+  using OpConversionPattern::OpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(FuncOp funcOp, FuncOp::Adaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    auto newFunc = rewriter.create<func::FuncOp>(
+        funcOp->getLoc(), funcOp.getSymName(), funcOp.getFunctionType());
+    rewriter.cloneRegionBefore(funcOp.getBody(), newFunc.getBody(),
+                               newFunc.getBody().end());
+    rewriter.replaceOp(funcOp, newFunc);
+    return success();
+  }
+};
+
+struct WasmReturnOpConversion : OpConversionPattern<ReturnOp> {
+  using OpConversionPattern::OpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(ReturnOp returnOp, ReturnOp::Adaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    rewriter.replaceOpWithNewOp<func::ReturnOp>(returnOp, adaptor.getOperands());
+    return success();
+  }
+};
+
 struct ConvertWasmToStandardPass
     : public impl::ConvertWasmToStandardBase<ConvertWasmToStandardPass> {
   void runOnOperation() override {
+    ConversionTarget target{getContext()};
+    target.addIllegalDialect<WasmDialect>();
+    target.addLegalDialect<BuiltinDialect, func::FuncDialect,
+                            memref::MemRefDialect>();
     RewritePatternSet patterns(&getContext());
-    populateWasmToStandardConversionPatterns(patterns);
-    if (failed(applyPatternsGreedily(getOperation(), std::move(patterns))))
+    TypeConverter tc{};
+    tc.addConversion([](Type type)->std::optional<Type>{
+      return type;
+    });
+
+    populateWasmToStandardConversionPatterns(tc, patterns);
+
+    if (failed(applyFullConversion(getOperation(), target, std::move(patterns))))
       return signalPassFailure();
   }
 };
 } // namespace
 
 void mlir::populateWasmToStandardConversionPatterns(
-    RewritePatternSet &) {
+    TypeConverter &tc, RewritePatternSet &patternSet) {
+  auto *ctx = patternSet.getContext();
+  patternSet.add<WasmFuncOpConversion, WasmReturnOpConversion>(tc, ctx);
 }
