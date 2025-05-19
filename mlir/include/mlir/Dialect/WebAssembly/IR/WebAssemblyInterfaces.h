fe786b1e33404d1ec41b96c33ec5355752bf4229
Luc Forget <luc.forget@woven.toyota>
2025-05-19 12:58:08 +0900

    [mlir][wasm] Common interface for import ops

---
+++ mlir/include/mlir/Dialect/WebAssembly/IR/WebAssemblyInterfaces.h
@@ -0,0 +1,21 @@
+//===- WebAssemblyInterfaces.h - WebAssembly Interfaces ---------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file defines op interfaces for the WebAssembly dialect in MLIR.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef MLIR_DIALECT_WEBASSEMBLY_IR_WEBASSEMBLYINTERFACES_H_
+#define MLIR_DIALECT_WEBASSEMBLY_IR_WEBASSEMBLYINTERFACES_H_
+
+#include "mlir/IR/BuiltinAttributes.h"
+#include "mlir/IR/Operation.h"
+
+#include "mlir/Dialect/WebAssembly/IR/WebAssemblyInterfaces.h.inc"
+
+#endif // MLIR_DIALECT_WEBASSEMBLY_IR_WEBASSEMBLYINTERFACES_H_
---
+++ mlir/lib/Dialect/WebAssembly/IR/WebAssemblyInterfaces.cpp
@@ -0,0 +1,16 @@
+//===- WebAssemblyInterfaces.cpp - WebAssembly Interfaces -------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file defines op interfaces for the WebAssembly dialect in MLIR.
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/Dialect/WebAssembly/IR/WebAssemblyInterfaces.h"
+#include "mlir/Dialect/WebAssembly/IR/WebAssembly.h"
+
+#include "mlir/Dialect/WebAssembly/IR/WebAssemblyInterfaces.cpp.inc"
