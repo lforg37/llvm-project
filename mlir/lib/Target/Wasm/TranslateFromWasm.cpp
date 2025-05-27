c7ea83bbbcc3231d108d0406627f0a3fe1d14464
Jessica Paquette (Woven by Toyota, Inc.)/Jessica Paquette <jessica.paquette@woven-planet.global>
2025-05-27 09:56:27 +0900

    [wasm] Translate + convert shr_u (#643)
    This adds translation and conversion to arith for
    
    * i32.shr_u
    * i64.shr_u
    
    SPL-6972
    
    Co-authored-by: Jessica Paquette <jessica.paquette@woven.toyota>

--- mlir/lib/Target/Wasm/TranslateFromWasm.cpp
+++ mlir/lib/Target/Wasm/TranslateFromWasm.cpp
@@ -135,6 +135,7 @@ struct WasmEncodings {
     static constexpr std::byte xorI32{0x73};
     static constexpr std::byte shlI32{0x74};
     static constexpr std::byte shr_sI32{0x75};
+    static constexpr std::byte shr_uI32{0x76};
 
     static constexpr std::byte eqI64{0x51};
     static constexpr std::byte neI64{0x52};
@@ -158,6 +159,7 @@ struct WasmEncodings {
     static constexpr std::byte xorI64{0x85};
     static constexpr std::byte shlI64{0x86};
     static constexpr std::byte shr_sI64{0x87};
+    static constexpr std::byte shr_uI64{0x88};
 
     static constexpr std::byte eqF32{0x5B};
     static constexpr std::byte neF32{0x5C};
@@ -1105,6 +1107,7 @@ ImplementNumericalOpInt(RemSIOp, remS)
 ImplementNumericalOpInt(RemUIOp, remU)
 ImplementNumericalOpInt(ShLOp, shl)
 ImplementNumericalOpInt(ShRSOp, shr_s)
+ImplementNumericalOpInt(ShRUOp, shr_u)
 ImplementNumericalOpInt(XOrOp, xor)
 ImplementNumericalOpInt(LtSIOp, ltS)
 ImplementNumericalOpInt(LeSIOp, leS)
---
+++ mlir/test/Target/WebAssembly/shr_u.mlir
@@ -0,0 +1,29 @@
+// RUN: mlir-translate --import-wasm %S/inputs/shr_u.wasm | FileCheck %s
+
+/* Source code used to generate this test:
+(module
+    (func (export "shr_u_i32") (result i32)
+    i32.const 10
+    i32.const 3
+    i32.shr_u)
+
+    (func (export "shr_u_i64") (result i64)
+    i64.const 10
+    i64.const 3
+    i64.shr_u)
+)
+*/
+
+// CHECK-LABEL: wasm.func @shr_u_i32() -> i32 {
+// CHECK:    %0 = wasm.const 10 : i32
+// CHECK:    %1 = wasm.const 3 : i32
+// CHECK:    %2 = wasm.shr_u %0 by %1 bits : i32
+// CHECK:    wasm.return %2 : i32
+
+// CHECK-LABEL: wasm.func @shr_u_i64() -> i64 {
+// CHECK:    %0 = wasm.const 10 : i64
+// CHECK:    %1 = wasm.const 3 : i64
+// CHECK:    %2 = wasm.shr_u %0 by %1 bits : i64
+// CHECK:    wasm.return %2 : i64
+
+
