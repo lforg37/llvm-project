8fbd00e556c5758f55be395eed05fac456dfd95c
Luc Forget <lforg37@users.noreply.github.com>
2025-06-25 13:03:55 +0200

    [mlir][wasm] Support for extend ops
    Support for two related but distinct kind of Wasm instructions.
    
    - `i64.extend_i32_sx` takes a 32 bits integer and widen it to 64 bits.
    - The various `inn.extendmm_s` takes an inn as input, and extend the mm low bits interpreted as 2's complement signed integer back to nn bits
    
    
    Co-authored-by: Ferdinand Lemaire <flemairen6@users.noreply.github.com>

--- mlir/include/mlir/Target/Wasm/WasmBinaryEncoding.h
+++ mlir/include/mlir/Target/Wasm/WasmBinaryEncoding.h
@@ -138,6 +138,8 @@ struct WasmBinaryEncoding {
     static constexpr std::byte wrap{0xA7};
 
     // Conversion operations
+    static constexpr std::byte extendS{0xAC};
+    static constexpr std::byte extendU{0xAD};
     static constexpr std::byte convertSI32F32{0xB2};
     static constexpr std::byte convertUI32F32{0xB3};
     static constexpr std::byte convertSI64F32{0xB4};
@@ -150,6 +152,12 @@ struct WasmBinaryEncoding {
     static constexpr std::byte convertSI64F64{0xB9};
     static constexpr std::byte convertUI64F64{0xBA};
     static constexpr std::byte promoteF32ToF64{0xBB};
+
+    static constexpr std::byte extendI328S{0xC0};
+    static constexpr std::byte extendI3216S{0xC1};
+    static constexpr std::byte extendI648S{0xC2};
+    static constexpr std::byte extendI6416S{0xC3};
+    static constexpr std::byte extendI6432S{0xC4};
   };
 
   /// Byte encodings of types in WASM binaries
