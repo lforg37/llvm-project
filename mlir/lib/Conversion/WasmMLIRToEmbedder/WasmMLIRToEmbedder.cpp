1ca1d570ab0ad5f3faeb03972303aa403304d4f5
Luc Forget <luc.forget@woven.toyota>
2025-07-11 15:01:25 +0900

    [mlir][wasm] Dummy conversion for trapping arithmetic ops
    Check overflow result for arithmetic ops that can overflow and trap if
    an overflow occured.
    
    Same for integer division if divider is zero.

--- mlir/lib/Conversion/WasmMLIRToEmbedder/WasmMLIRToEmbedder.cpp
+++ mlir/lib/Conversion/WasmMLIRToEmbedder/WasmMLIRToEmbedder.cpp
@@ -15,15 +15,22 @@
 #include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/ControlFlow/IR/ControlFlowOps.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/LLVMIR/LLVMTypes.h"
 #include "mlir/Dialect/Math/IR/Math.h"
 #include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
 #include "mlir/Dialect/Vector/IR/VectorOps.h"
 #include "mlir/Dialect/WebAssembly/IR/WebAssembly.h"
-#include "mlir/Dialect/WebAssembly/IR/WebAssemblyInterfaces.h"
+#include "mlir/IR/Block.h"
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/BuiltinAttributes.h"
 #include "mlir/IR/BuiltinDialect.h"
+#include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/MLIRContext.h"
 #include "mlir/IR/SymbolTable.h"
+#include "mlir/IR/TypeRange.h"
 #include "mlir/IR/Types.h"
+#include "mlir/IR/Value.h"
 #include "mlir/IR/ValueRange.h"
 #include "mlir/Transforms/DialectConversion.h"
 #include "mlir/Transforms/Passes.h"
@@ -31,6 +38,7 @@
 #include "llvm/Support/Casting.h"
 #include "llvm/Support/ErrorHandling.h"
 #include "llvm/Support/LogicalResult.h"
+#include <array>
 #include <string>
 
 #define DEBUG_TYPE "wasm-convert"
@@ -45,64 +53,135 @@ using namespace mlir::wasm;
 
 namespace {
 
-static Region *getGlobalRegion(Operation *op) {
-  auto *currentReg = op->getParentRegion();
-  while (currentReg->getParentRegion() != nullptr)
-    currentReg = currentReg->getParentRegion();
-  return currentReg;
-}
-
-static StringLiteral getTypeString(mlir::Type t) {
-  if (t.isF32())
-    return "f32";
-  if (t.isF64())
-    return "f64";
-  if (t.isInteger(32))
-    return "i32";
-  if (t.isInteger(64))
-    return "i64";
-  llvm_unreachable("unsupported datatype by the wasm spec");
-}
-
-template <typename SourceOp>
-struct TrappableOpConversion : OpConversionPattern<SourceOp> {
-  using OpConversionPattern<SourceOp>::OpConversionPattern;
-
-  LogicalResult
-  matchAndRewrite(SourceOp srcOp, typename SourceOp::Adaptor adaptor,
-                  ConversionPatternRewriter &rewriter) const override {
-    MLIRContext *ctx = srcOp.getContext();
-    auto *symTable = SymbolTable::getNearestSymbolTable(srcOp);
-    auto opName = srcOp.getOperationName();
-    auto typeName = getTypeString(srcOp.getResult().getType());
-    std::string symbolName = opName.str() + "_" + typeName.str();
-    if (!symTable)
-      return failure();
-    if (!SymbolTable::lookupSymbolIn(symTable, symbolName)) {
-      auto ip = rewriter.saveInsertionPoint();
-      auto *globRegion = getGlobalRegion(srcOp);
-      rewriter.setInsertionPoint(&globRegion->getBlocks().begin()->front());
-      auto funcDec = rewriter.create<func::FuncOp>(
-          globRegion->getLoc(), symbolName,
-          FunctionType::get(ctx, srcOp->getOperandTypes(),
-                            srcOp->getResultTypes()));
-      SymbolTable::setSymbolVisibility(funcDec,
-                                       SymbolTable::Visibility::Private);
-      rewriter.restoreInsertionPoint(ip);
-    }
-    rewriter.replaceOpWithNewOp<func::CallOp>(
-        srcOp, symbolName, srcOp.getResult().getType(), srcOp.getOperands());
-
-    return success();
-  }
+struct WasmRuntimeFunctionsSymbols {
+  SymbolRefAttr trap;
 };
 
-using WasmAddConversion = TrappableOpConversion<AddOp>;
-using WasmDivConversion = TrappableOpConversion<DivOp>;
-using WasmDivUIConversion = TrappableOpConversion<DivUIOp>;
-using WasmDivSIConversion = TrappableOpConversion<DivSIOp>;
-using WasmMulConversion = TrappableOpConversion<MulOp>;
-using WasmSubConversion = TrappableOpConversion<SubOp>;
+template <typename SrcOp>
+struct WasmRuntimeOpConversion : OpConversionPattern<SrcOp> {
+  WasmRuntimeOpConversion(MLIRContext *context,
+                          WasmRuntimeFunctionsSymbols const &runtimeFuncSym)
+      : OpConversionPattern<SrcOp>{context}, runtimeSymbols{runtimeFuncSym} {}
+  WasmRuntimeOpConversion(const TypeConverter &typeConverter,
+                          MLIRContext *context,
+                          WasmRuntimeFunctionsSymbols const &runtimeFuncSym)
+      : OpConversionPattern<SrcOp>{typeConverter, context},
+        runtimeSymbols{runtimeFuncSym} {}
+
+public:
+  WasmRuntimeFunctionsSymbols const &runtimeSymbols;
+};
+
+template<typename WasmOpType, typename ArithOpName>
+struct DivOpConversion : WasmRuntimeOpConversion<WasmOpType> {
+  using WasmRuntimeOpConversion<WasmOpType>::WasmRuntimeOpConversion;
+  LogicalResult
+  matchAndRewrite(WasmOpType divOp, typename WasmOpType::Adaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    auto addResType = divOp.getResult().getType();
+    auto divByZeroMarkerType = rewriter.getIntegerType(1);
+    auto zero = rewriter.create<arith::ConstantOp>(
+        divOp->getLoc(), rewriter.getIntegerAttr(addResType, 0));
+    auto isDividerZero = rewriter.create<arith::CmpIOp>(
+        divOp.getLoc(), divByZeroMarkerType, arith::CmpIPredicate::eq,
+        adaptor.getRhs(), zero.getResult());
+    auto *curBlock = divOp->getBlock();
+    auto callTrap = rewriter.create<func::CallOp>(
+        divOp->getLoc(), this->runtimeSymbols.trap, TypeRange{}, ValueRange{});
+    Block *trapBlock =
+        rewriter.splitBlock(curBlock, Block::iterator(callTrap.getOperation()));
+    Block *normalBlock =
+        rewriter.splitBlock(trapBlock, ++Block::iterator{callTrap});
+    rewriter.setInsertionPointAfter(callTrap);
+    rewriter.create<cf::BranchOp>(divOp->getLoc(), normalBlock);
+    rewriter.setInsertionPointAfter(isDividerZero);
+    rewriter.create<cf::CondBranchOp>(divOp->getLoc(), isDividerZero.getResult(),
+                                      trapBlock, ValueRange{}, normalBlock,
+                                      ValueRange{});
+    rewriter.setInsertionPointToStart(normalBlock);
+    rewriter.replaceOpWithNewOp<ArithOpName>(divOp, adaptor.getLhs(), adaptor.getRhs());
+    return success();
+  };
+};
+
+using DivSITrapDivZeroConversion = DivOpConversion<DivSIOp, arith::DivSIOp>;
+using DivUITrapDivZeroConversion = DivOpConversion<DivUIOp, arith::DivUIOp>;
+
+template <typename WasmOpType, typename IntrinsicOpType>
+struct ArithToIntrinsicTrapOpConversion : WasmRuntimeOpConversion<WasmOpType> {
+  using WasmRuntimeOpConversion<WasmOpType>::WasmRuntimeOpConversion;
+  LogicalResult
+  matchAndRewrite(WasmOpType wasmOp, typename WasmOpType::Adaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    if (!wasmOp.getRhs().getType().isInteger())
+      return rewriter.notifyMatchFailure(
+          wasmOp->getLoc(),
+          "This pattern handles only add operations om integer operands");
+    auto addResType = wasmOp.getResult().getType();
+    auto overflowMarkerType = rewriter.getIntegerType(1);
+    auto intrinsicResType = LLVM::LLVMStructType::getLiteral(
+        this->getContext(), {addResType, overflowMarkerType});
+    auto intrinsicCall = rewriter.create<IntrinsicOpType>(
+        wasmOp->getLoc(), intrinsicResType, adaptor.getLhs(), adaptor.getRhs());
+    auto result =
+        cast<TypedValue<LLVM::LLVMStructType>>(intrinsicCall.getResult());
+    auto addResult = rewriter.create<LLVM::ExtractValueOp>(
+        wasmOp->getLoc(), addResType, result,
+        rewriter.getDenseI64ArrayAttr({0}));
+    auto overflowMarker = rewriter.create<LLVM::ExtractValueOp>(
+        wasmOp->getLoc(), overflowMarkerType, result,
+        rewriter.getDenseI64ArrayAttr({1}));
+    auto *curBlock = wasmOp->getBlock();
+    auto callTrap = rewriter.create<func::CallOp>(
+        wasmOp->getLoc(), this->runtimeSymbols.trap, TypeRange{}, ValueRange{});
+    Block *trapBlock =
+        rewriter.splitBlock(curBlock, Block::iterator(callTrap.getOperation()));
+    Block *normalBlock =
+        rewriter.splitBlock(trapBlock, ++Block::iterator{callTrap});
+    rewriter.setInsertionPointAfter(callTrap);
+    rewriter.create<cf::BranchOp>(wasmOp->getLoc(), normalBlock);
+    rewriter.setInsertionPointAfter(overflowMarker);
+    rewriter.create<cf::CondBranchOp>(wasmOp->getLoc(), overflowMarker.getRes(),
+                                      trapBlock, ValueRange{}, normalBlock,
+                                      ValueRange{});
+    rewriter.replaceOp(wasmOp, addResult.getRes());
+    return success();
+  };
+};
+
+using AddOpConversion =
+    ArithToIntrinsicTrapOpConversion<AddOp, LLVM::UAddWithOverflowOp>;
+using MulOpConversion =
+    ArithToIntrinsicTrapOpConversion<MulOp, LLVM::UMulWithOverflowOp>;
+using SubOpConversion =
+    ArithToIntrinsicTrapOpConversion<SubOp, LLVM::USubWithOverflowOp>;
+
+WasmRuntimeFunctionsSymbols insertEmbedderFuncDeclarations(ModuleOp module) {
+  WasmRuntimeFunctionsSymbols res;
+  OpBuilder opBuilder{module.getBodyRegion()};
+  auto trapDesc = opBuilder.create<func::FuncOp>(
+      module.getBodyRegion().getLoc(), "wasm.trap",
+      FunctionType::get(module->getContext(), TypeRange{}, TypeRange{}));
+  SymbolTable::setSymbolVisibility(trapDesc, SymbolTable::Visibility::Private);
+  res.trap = SymbolRefAttr::get(trapDesc);
+  return res;
+}
+
+void populateWasmMLIRToEmbedderConversionPatterns(
+    TypeConverter &tc, RewritePatternSet &patternSet,
+    WasmRuntimeFunctionsSymbols const &runtimeSymbols) {
+  auto *ctx = patternSet.getContext();
+  // Disable clang-format in patternSet for readability + small diffs.
+  // clang-format off
+  patternSet.add<
+      AddOpConversion,
+      DivSITrapDivZeroConversion,
+      DivUITrapDivZeroConversion,
+      MulOpConversion,
+      SubOpConversion
+  >(tc, ctx, runtimeSymbols);
+  // clang-format on
+}
 
 struct WasmMLIRToEmbedderPass
     : public impl::WasmMLIRToEmbedderBase<WasmMLIRToEmbedderPass> {
@@ -111,32 +190,20 @@ struct WasmMLIRToEmbedderPass
     RewritePatternSet patterns(&getContext());
     target.addLegalDialect<arith::ArithDialect, BuiltinDialect,
                            cf::ControlFlowDialect, func::FuncDialect,
-                           memref::MemRefDialect, math::MathDialect>();
+                           LLVM::LLVMDialect, memref::MemRefDialect,
+                           math::MathDialect>();
     TypeConverter tc{};
     tc.addConversion([](Type type) -> std::optional<Type> { return type; });
-    populateWasmMLIRToEmbedderConversionPatterns(tc, patterns);
-    auto *head = getOperation();
-    if (failed(applyFullConversion(head, target, std::move(patterns))))
+    auto module = dyn_cast<ModuleOp>(getOperation());
+    if (! module)
+      return signalPassFailure();
+    auto runtimeSymbols = insertEmbedderFuncDeclarations(module);
+    populateWasmMLIRToEmbedderConversionPatterns(tc, patterns, runtimeSymbols);
+    if (failed(applyFullConversion(module, target, std::move(patterns))))
       return signalPassFailure();
   }
 };
 } // namespace
-
-void mlir::populateWasmMLIRToEmbedderConversionPatterns(
-    TypeConverter &tc, RewritePatternSet &patternSet) {
-  auto *ctx = patternSet.getContext();
-  // Disable clang-format in patternSet for readability + small diffs.
-  // clang-format off
-  patternSet.add<
-      WasmAddConversion,
-      WasmDivConversion,
-      WasmDivSIConversion,
-      WasmDivUIConversion,
-      WasmMulConversion,
-      WasmSubConversion
-  >(tc, ctx);
-  // clang-format on
-}
 
 std::unique_ptr<Pass> mlir::createWasmMLIRToEmbedderPass() {
   return std::make_unique<WasmMLIRToEmbedderPass>();
--- mlir/test/Conversion/WasmMLIRToEmbedder/wasm-mul-embedder-mul.mlir
+++ mlir/test/Conversion/WasmMLIRToEmbedder/wasm-mul-embedder-mul.mlir
@@ -1,4 +1,6 @@
-// RUN: mlir-opt --split-input-file %s --raise-wasm-mlir --wasm-mlir-to-embedder | FileCheck %s
+// RUN: mlir-opt %s --raise-wasm-mlir --wasm-mlir-to-embedder | FileCheck %s
+
+// CHECK:   func.func private @wasm.trap()
 
 wasm.func nested @mul_i32(%arg0: !wasm<local ref to i32>, %arg1: !wasm<local ref to i32>) -> i32 {
     %v0 = wasm.local_get %arg0 : ref to i32
@@ -6,8 +8,6 @@ wasm.func nested @mul_i32(%arg0: !wasm<local ref to i32>, %arg1: !wasm<local ref
     %0 = wasm.mul %v0 %v1 : i32
     wasm.return %0 : i32
 }
-
-// CHECK-LABEL:   func.func private @wasm.mul_i32(i32, i32) -> i32
 
 // CHECK-LABEL:   func.func @mul_i32(
 // CHECK-SAME:      %[[ARG0:.*]]: i32,
@@ -18,10 +18,15 @@ wasm.func nested @mul_i32(%arg0: !wasm<local ref to i32>, %arg1: !wasm<local ref
 // CHECK:           memref.store %[[ARG0]], %[[VAL_1]][] : memref<i32>
 // CHECK:           %[[VAL_2:.*]] = memref.load %[[VAL_1]][] : memref<i32>
 // CHECK:           %[[VAL_3:.*]] = memref.load %[[VAL_0]][] : memref<i32>
-// CHECK:           %[[VAL_4:.*]] = call @wasm.mul_i32(%[[VAL_2]], %[[VAL_3]]) : (i32, i32) -> i32
-// CHECK:           return %[[VAL_4]] : i32
-
-// -----
+// CHECK:           %[[VAL_4:.*]] = "llvm.intr.umul.with.overflow"(%[[VAL_2]], %[[VAL_3]]) : (i32, i32) -> !llvm.struct<(i32, i1)>
+// CHECK:           %[[VAL_5:.*]] = llvm.extractvalue %[[VAL_4]][0] : !llvm.struct<(i32, i1)>
+// CHECK:           %[[VAL_6:.*]] = llvm.extractvalue %[[VAL_4]][1] : !llvm.struct<(i32, i1)>
+// CHECK:           cf.cond_br %[[VAL_6]], ^bb1, ^bb2
+// CHECK:         ^bb1:
+// CHECK:           call @wasm.trap() : () -> ()
+// CHECK:           cf.br ^bb2
+// CHECK:         ^bb2:
+// CHECK:           return %[[VAL_5]] : i32
 
 wasm.func nested @mul_i64(%arg0: !wasm<local ref to i64>, %arg1: !wasm<local ref to i64>) -> i64 {
     %v0 = wasm.local_get %arg0 : ref to i64
@@ -29,8 +34,6 @@ wasm.func nested @mul_i64(%arg0: !wasm<local ref to i64>, %arg1: !wasm<local ref
     %0 = wasm.mul %v0 %v1 : i64
     wasm.return %0 : i64
 }
-
-// CHECK:         func.func private @wasm.mul_i64(i64, i64) -> i64
 
 // CHECK-LABEL:   func.func @mul_i64(
 // CHECK-SAME:      %[[ARG0:.*]]: i64,
@@ -41,5 +44,12 @@ wasm.func nested @mul_i64(%arg0: !wasm<local ref to i64>, %arg1: !wasm<local ref
 // CHECK:           memref.store %[[ARG0]], %[[VAL_1]][] : memref<i64>
 // CHECK:           %[[VAL_2:.*]] = memref.load %[[VAL_1]][] : memref<i64>
 // CHECK:           %[[VAL_3:.*]] = memref.load %[[VAL_0]][] : memref<i64>
-// CHECK:           %[[VAL_4:.*]] = call @wasm.mul_i64(%[[VAL_2]], %[[VAL_3]]) : (i64, i64) -> i64
-// CHECK:           return %[[VAL_4]] : i64
+// CHECK:           %[[VAL_4:.*]] = "llvm.intr.umul.with.overflow"(%[[VAL_2]], %[[VAL_3]]) : (i64, i64) -> !llvm.struct<(i64, i1)>
+// CHECK:           %[[VAL_5:.*]] = llvm.extractvalue %[[VAL_4]][0] : !llvm.struct<(i64, i1)>
+// CHECK:           %[[VAL_6:.*]] = llvm.extractvalue %[[VAL_4]][1] : !llvm.struct<(i64, i1)>
+// CHECK:           cf.cond_br %[[VAL_6]], ^bb1, ^bb2
+// CHECK:         ^bb1:
+// CHECK:           call @wasm.trap() : () -> ()
+// CHECK:           cf.br ^bb2
+// CHECK:         ^bb2:
+// CHECK:           return %[[VAL_5]] : i64
