27b360ac26622a5e76d826ff913a14a95f8e849b
Ferdinand Lemaire <ferdinand.lemaire@woven-planet.global>
2025-07-14 11:25:53 +0900

    [mlir][wasm] Introduce trap Op and move the lowering logic of trappable ops back to RaiseWasm.

--- mlir/lib/Dialect/WebAssembly/IR/WebAssemblyOps.cpp
+++ mlir/lib/Dialect/WebAssembly/IR/WebAssemblyOps.cpp
@@ -67,14 +67,6 @@ ParseResult parseImportOp(OpAsmParser &parser, OperationState &result) {
 } // namespace
 
 //===----------------------------------------------------------------------===//
-// AddOp
-//===----------------------------------------------------------------------===//
-
-LogicalResult AddOp::verifyNeedToInsertTrapOp() {
-   return success(!this->getResult().getType().isFloat());
-};
-
-//===----------------------------------------------------------------------===//
 // BlockOp
 //===----------------------------------------------------------------------===//
 
@@ -470,14 +462,6 @@ void MemImportOp::build(mlir::OpBuilder &odsBuilder,
 }
 
 //===----------------------------------------------------------------------===//
-// MulOp
-//===----------------------------------------------------------------------===//
-
-LogicalResult MulOp::verifyNeedToInsertTrapOp() {
-   return success(!this->getResult().getType().isFloat());
-};
-
-//===----------------------------------------------------------------------===//
 // ReinterpretOp
 //===----------------------------------------------------------------------===//
 
@@ -498,14 +482,6 @@ LogicalResult ReinterpretOp::verify() {
 
 void ReturnOp::build(::mlir::OpBuilder &odsBuilder,
                      ::mlir::OperationState &odsState) {}
-
-//===----------------------------------------------------------------------===//
-// SubOp
-//===----------------------------------------------------------------------===//
-
-LogicalResult SubOp::verifyNeedToInsertTrapOp() {
-   return success(!this->getResult().getType().isFloat());
-};
 
 //===----------------------------------------------------------------------===//
 // TableOp
